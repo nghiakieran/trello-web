1e46ab1a9a77e3d62cc6ab0ca15348ad9cd18f49
nghiakieran <lechinghia202@gmail.com>
2025-01-19 22:10:40 +0700

    Add config MUI Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 function App() {
