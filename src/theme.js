282ec8df8acf5c132460604c640f6e239f6ee6c6
nghiakieran <lechinghia202@gmail.com>
2025-02-09 23:32:58 +0700

    fix avatar and scrollbar

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,3 @@
-// import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 const theme = extendTheme({
   trello: {
@@ -28,11 +27,11 @@ const theme = extendTheme({
             height: '8px'
           },
           '*::-webkit-scrollbar-thumb': {
-            backgroundColor: '#bdc3c7',
+            backgroundColor: '#dcdde1',
             borderRadius: '8px'
           },
           '*::-webkit-scrollbar-thumb:hover': {
-            backgroundColor: '#00b894'
+            backgroundColor: 'white'
           }
         }
       }
@@ -48,32 +47,18 @@ const theme = extendTheme({
     },
     MuiInputLabel: {
       styleOverrides: {
-        root: ({ theme }) => {
-          return {
-            // color: theme.palette.primary.main,
-            fontSize: '0.875rem'
-          }
+        root: {
+          fontSize: '0.875rem'
         }
       }
     },
     MuiOutlinedInput: {
       styleOverrides: {
-        root: ({ theme }) => {
-          return {
-            // color: theme.palette.primary.main,
-            fontSize: '0.875rem',
-            // '.MuiOutlinedInput-notchedOutline': {
-            //   borderColor: theme.palette.primary.light
-            // },
-            // '&:hover': {
-            //   '.MuiOutlinedInput-notchedOutline': {
-            //     borderColor: theme.palette.primary.main
-            //   }
-            // },
-            '& fieldset': { borderWidth: '0.5px !important' },
-            '&:hover fieldset': { borderWidth: '1px !important' },
-            '&.Mui-focused fieldset': { borderWidth: '1px !important' }
-          }
+        root: {
+          fontSize: '0.875rem',
+          '& fieldset': { borderWidth: '0.5px !important' },
+          '&:hover fieldset': { borderWidth: '1px !important' },
+          '&.Mui-focused fieldset': { borderWidth: '1px !important' }
         }
       }
     }
