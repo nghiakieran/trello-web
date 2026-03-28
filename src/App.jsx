286b06c8700fd4931bbb5b50dd15f0b3e4d06fb0
nghiakieran <lechinghia202@gmail.com>
2026-03-28 15:39:46 +0700

    Add ui and validate form login, resgister

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Board from '~/pages/Boards/_id'
 import NotFound from './pages/404/NotFound'
+import Auth from './pages/Auth/Auth'
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <Route path='/' element={
         <Navigate to='/boards/68b3cd8fcbf6838882060c3c' replace />
       } />
+
+      <Route path='/login' element={<Auth />} />
+      <Route path='/register' element={<Auth />} />
       <Route path='/boards/:boardId' element={<Board />} />
 
       <Route path='*' element={<NotFound />} />
