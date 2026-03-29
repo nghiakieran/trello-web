9917ca8bff6f51af1cf16267e264d696038a74b5
nghiakieran <lechinghia202@gmail.com>
2026-03-29 22:59:41 +0700

    Add redux persist for save localStorage user infomation

--- src/main.jsx
+++ src/main.jsx
@@ -11,22 +11,29 @@ import { Provider } from 'react-redux'
 import { store } from '~/redux/store'
 import { BrowserRouter } from 'react-router-dom'
 
+// redux persist
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+const persistor = persistStore(store)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename='/'>
     <Provider store={store}>
-      <CssVarsProvider theme={theme}>
-        <ConfirmProvider defaultOptions={{
-          allowClose: false,
-          dialogProps: { maxWidth: 'xs' },
-          buttonOrder: ['confirm', 'cancel'],
-          cancellationButtonProps: { color: 'inherit' },
-          confirmationButtonProps: { color: 'secondary', variant: 'outlined' }
-        }}>
-          <CssBaseline />
-          <App />
-          <ToastContainer position='bottom-left' theme='colored' />
-        </ConfirmProvider>
-      </CssVarsProvider>
+      <PersistGate persistor={persistor}>
+        <CssVarsProvider theme={theme}>
+          <ConfirmProvider defaultOptions={{
+            allowClose: false,
+            dialogProps: { maxWidth: 'xs' },
+            buttonOrder: ['confirm', 'cancel'],
+            cancellationButtonProps: { color: 'inherit' },
+            confirmationButtonProps: { color: 'secondary', variant: 'outlined' }
+          }}>
+            <CssBaseline />
+            <App />
+            <ToastContainer position='bottom-left' theme='colored' />
+          </ConfirmProvider>
+        </CssVarsProvider>
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import { activeBoardReducer } from './slice/activeBoardSlice'
 import { userReducer } from './slice/userSlice'
 
+const rootPersistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ['user'] // slice user storaged localstorage
+}
+
+const reducers = combineReducers({
+  activeBoard: activeBoardReducer,
+  user: userReducer
+})
+
+const persistedReducer = persistReducer(rootPersistConfig, reducers)
+
 export const store = configureStore({
-  reducer: {
-    activeBoard: activeBoardReducer,
-    user: userReducer
-  }
+  reducer: persistedReducer
 })
