14f8751e57309743efc4aea81639ca929bf1a5fc
nghiakieran <lechinghia202@gmail.com>
2026-03-30 12:35:40 +0700

    Fix warning redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -20,5 +20,6 @@ const reducers = combineReducers({
 const persistedReducer = persistReducer(rootPersistConfig, reducers)
 
 export const store = configureStore({
-  reducer: persistedReducer
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
 })
